977a8ca73eec7b071af6577a8424268d2e88835f
helenamihaljevic <helena.mihaljevic@posteo.net>
2018-04-16 11:47:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(name='name-gender-inference',
+      version='0.1',
+      description='Code and data for evaluation of name-based gender inference tools',
+      url='https://github.com/GenderGapSTEM-PublicationAnalysis/name-gender-inference',
+      author='Helena Mihaljević and Lucia Santamaría',
+      license='MIT',
+      packages=['name-gender-inference'],
+      zip_safe=False)
