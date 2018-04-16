22d6afeceea7ab9cc2edd1cac3bff8ba49f79548
helenamihaljevic <helena.mihaljevic@posteo.net>
2018-04-16 12:12:43 +0200

    Rename library root for packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from setuptools import setup
 
-setup(name='name-gender-inference',
+setup(name='name_gender_inference',
       version='0.1',
       description='Code and data for evaluation of name-based gender inference tools',
       url='https://github.com/GenderGapSTEM-PublicationAnalysis/name-gender-inference',
